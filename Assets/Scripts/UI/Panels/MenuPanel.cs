1619de6f881966857ec8632bd9552b8c67bba493
cronrpc <147160173+cronrpc@users.noreply.github.com>
2025-10-03 12:59:00 +0800

    [Feature]: Use canvasgroup to achieve a fade-in effect.

--- Assets/Scripts/UI/Panels/MenuPanel.cs
+++ Assets/Scripts/UI/Panels/MenuPanel.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Timers;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +9,13 @@ namespace UI.Panels
     {
         [SerializeField] private Button openNewPanel;
         [SerializeField] private Button openUserPanel;
-        
+        private CanvasGroup _canvasGroup;
+
+        private void Awake()
+        {
+            _canvasGroup = GetComponent<CanvasGroup>();
+        }
+
         void Start()
         {
             openNewPanel.onClick.AddListener((() =>
@@ -18,6 +26,22 @@ namespace UI.Panels
             {
                 UIManager.Instance.OpenPanel(UIPanelType.UserPanel);
             }));
+        }
+
+        public override void OpenPanel(UIPanelType panelType)
+        {
+            base.OpenPanel(panelType);
+            _canvasGroup.alpha = 0;
+        }
+
+        private float timer = 0;
+        private void Update()
+        {
+            if (timer < 1.1)
+            {
+                timer += Time.deltaTime;
+                _canvasGroup.alpha = Mathf.Clamp01(timer);
+            }
         }
     }
 }
